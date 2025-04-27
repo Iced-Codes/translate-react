2c9328b2731855a4d88ac5b88d6f7beb7ec15a1a
icdecode <icdecode@163.com>
2025-04-27 21:30:33 +0800

    feat：初始化react翻译项目

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import autoImport from "unplugin-auto-import/vite";
+import unocss from "unocss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    unocss(),
+    autoImport({
+      dts: "./src/auto-imports.d.ts",
+      imports: ["react", "ahooks", "react-router", "react-router-dom"],
+    }),
+  ],
+  server: {
+    port: 3000,
+    open: true,
+    host: "0.0.0.0",
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
